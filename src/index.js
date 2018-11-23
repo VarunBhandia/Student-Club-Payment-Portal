9ce283f20ea4946de46b348f8d303f6acada5a32
BhaveshPatidar <bhavesh1999@gmail.com>
2018-11-23 17:07:12 +0530

    Navbar added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavbarHeader from './mainpage';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <NavbarHeader />,
+  document.getElementById('root')
+);
