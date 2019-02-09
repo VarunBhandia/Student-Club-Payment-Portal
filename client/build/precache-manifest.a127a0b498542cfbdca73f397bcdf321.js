2e84b4f3ef38e1729fcaccf90e0346f0d9eba12c
BhaveshPatidar <bhavesh1999@gmail.com>
2019-02-09 21:55:40 +0530

    resolved multiple booking issue

---
+++ client/build/precache-manifest.a127a0b498542cfbdca73f397bcdf321.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "424e45de5baa66613185aeae80d940e7",
+    "url": "/static/media/snooker.424e45de.jpg"
+  },
+  {
+    "revision": "44e0bf0a9b92bcc3ce97253f73b8a78b",
+    "url": "/static/media/playstation.44e0bf0a.jpg"
+  },
+  {
+    "revision": "d7f4c912c647cca8f26decb325aa278a",
+    "url": "/static/media/output-0.d7f4c912.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2482c8497fd8e477ec03",
+    "url": "/static/js/main.2482c849.chunk.js"
+  },
+  {
+    "revision": "5d6604fcb56a8df47c9f",
+    "url": "/static/js/1.5d6604fc.chunk.js"
+  },
+  {
+    "revision": "2482c8497fd8e477ec03",
+    "url": "/static/css/main.a2a0a0d2.chunk.css"
+  },
+  {
+    "revision": "384e1a4693eb59ed359fec0c5f7b5a5a",
+    "url": "/index.html"
+  }
+];
