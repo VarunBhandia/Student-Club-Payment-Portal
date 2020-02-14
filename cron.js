d29c14fd8612c75b39044e18cdf54707d5bf73fb
Harshit Khetan <khetanmehul@gmail.com>
2020-02-15 02:49:43 +0530

    Change cron job task

--- cron.js
+++ cron.js
@@ -1,1 +1,11 @@
-console.log("Clearing db values");
+import { MongoClient } from 'mongodb';
+
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/heroku_zt5h9gdm';
+
+MongoClient.connect(url, { useNewUrlParser: true}, (err, db) => { 
+    if(err) throw err;
+    var dbo = db.db(process.env.DB_NAME);
+    dbo.collection("TableStatus").deleteMany({});
+    dbo.collection("OrderTableStatus").deleteMany({});
+    console.log("Database values cleared");
+})
