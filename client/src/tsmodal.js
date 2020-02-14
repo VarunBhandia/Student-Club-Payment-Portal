3b714232d9f89b1a03c1fa40c76d82b573e20837
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-02-14 18:17:31 +0530

    Feature: added time conditional rendering of time slots in pool and playstation booking portal (#15)

--- client/src/tsmodal.js
+++ client/src/tsmodal.js
@@ -96,16 +96,22 @@ class TsModal extends React.Component {
           OPTIONS.splice(index, 1);
         }
       }
-      OPTIONS = OPTIONS.map(value => {
+      var AVAILABLE_OPTIONS= [];
+      for (let i = 0; i < OPTIONS.length; i++) {
+        var slot = OPTIONS[i];
+        var currentDate = new Date(); 
+        var currentTime =   currentDate.getHours() + ":" + currentDate.getMinutes();
+        var timeString = slot.split("-");
+        if(currentTime < timeString[0])
+          AVAILABLE_OPTIONS[i]=OPTIONS[i]
+      }
+      OPTIONS = AVAILABLE_OPTIONS.map(value => {
         return  ( <label style={{marginRight:'10px'}} for="mybox">{value}<li><br></br><input  onChange={this.handleCheckbox} type="checkbox" id="mybox"  name = 'unchecked' value={value + " " + currentT.currentTable}  /></li></label> 
         );
       });
     }
   }
-  
   handleTotalBooking(item,str,amount) {
-    
-    // eslint-disable-next-line
     this.setState({currentTable:str, currentAmount:amount, dis:false})
     var newArr = this.state.finalbook;
     var totamo = this.state.totalamount;
@@ -132,7 +138,6 @@ class TsModal extends React.Component {
 
   
   render() {
-    
     var  { finalbook, totalamount, currentTable, currentAmount} = this.state;
     var final = this.state.finalbook;
     final = final.map(function(value){
