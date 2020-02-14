a066856c3a8a3e7cb8d8270bdd584dc56ce0f758
Harshit Khetan <khetanmehul@gmail.com>
2020-02-15 03:10:18 +0530

    Testing cron job

--- cron.js
+++ cron.js
@@ -1,6 +1,7 @@
-import { MongoClient } from 'mongodb';
+var MongoClient = require('mongodb').MongoClient;
 
 var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/heroku_zt5h9gdm';
+console.log(process.env.MONGOLAB_URI);
 
 MongoClient.connect(url, { useNewUrlParser: true}, (err, db) => { 
     if(err) throw err;
